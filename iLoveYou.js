e1f09efa41e5099ca960dfd258936578c0d1daf4
Ayra Moraes <ayraamerico@gmail.com>
2023-02-02 15:10:36 +0000

    Excercise to spell a text and repeat a name N times

---
+++ iLoveYou.js
@@ -0,0 +1,13 @@
+function sayILoveYou(number) {
+    
+    let iLoveYou = "I love You"
+    for (let i = 0; i < number; i++) {
+      
+        console.log(iLoveYou);
+    
+    }
+
+
+  }
+sayILoveYou(34)
+
---
+++ spellText.js
@@ -0,0 +1,15 @@
+function spellText(text) {
+    for (let i = 0 ; i < text.length ; i++) {
+        console.log(text.charAt(i))
+    }
+
+    // for (let letter of text) {
+    //     console.log(letter)
+    // }
+
+}
+spellText("Ayra")
+
+/* i = i + 1
+i += 1
+i++ */

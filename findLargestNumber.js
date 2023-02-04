75c0e93194f4fa30d2fd58e071373a5730b37612
Ayra Moraes <ayraamerico@gmail.com>
2023-02-04 06:53:17 +0000

    receive an array of numbers and return the largest one

---
+++ findLargestNumber.js
@@ -0,0 +1,19 @@
+// função que vai retornar o maior número dos arrays
+// Comparar o primeiro elemento do array com a variavel maior. 
+
+// Se o elemento foi maior que o maior, ele substitui o elemento 
+
+function findLargestNumber(arrayNumber) {
+
+  let largest = arrayNumber[0]
+
+  for (let i = 1; i < arrayNumber.length; i++) {
+    if (arrayNumber[i] > largest) {
+      largest = arrayNumber[i];
+    }
+  }
+  return largest;
+}
+
+
+console.log(findLargestNumber([5, 8,]))

fb40a1975cae71f2df5d51016fd18affd51a7483
Ayra Moraes <ayraamerico@gmail.com>
2023-03-15 17:19:56 +0000

    Modifications on the Modal archive, making more than one Modal

--- course-javascript/modalWindow/script.js
+++ course-javascript/modalWindow/script.js
@@ -1,29 +1,41 @@
 'use strict';
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const buttonCloseModal = document.querySelector('.close-modal');
+const modals = document.querySelectorAll('.modal');
+const overlays = document.querySelectorAll('.overlay');
+const buttonCloseModals = document.querySelectorAll('.close-modal');
 const buttonOpenModal = document.querySelectorAll('.show-modal');
 
-const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+const openModal = function (index) {
+  modals[index].classList.remove('hidden');
+  overlays[index].classList.remove('hidden');
 };
 
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+const closeModal = function (index) {
+  modals[index].classList.add('hidden');
+  overlays[index].classList.add('hidden');
 };
 
-buttonOpenModal.forEach(function(button) {
-  button.addEventListener('click', openModal);
+buttonOpenModal.forEach(function (button, index) {
+  button.addEventListener('click', function () {
+    openModal(index);
+  });
 });
 
-buttonCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+buttonCloseModals.forEach(function (button, index) {
+  button.addEventListener('click', function () {
+    closeModal(index);
+  });
+});
 
-document.addEventListener('keydown', function (e) {
-   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
+overlays.forEach(function (overlay, index) {
+  overlay.addEventListener('click', function () {
+    closeModal(index);
+  });
+});
+
+modals.forEach(function (modal, index) {
+  if (!modal.classList.contains('hidden')) {
+    closeModal(index);
+    return; 
   }
 });

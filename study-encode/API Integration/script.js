80cec8e85745199785c06202187f2e8461efd28e
Ayra Moraes <ayraamerico@gmail.com>
2023-04-11 17:18:42 +0100

    Implamanting Search for Activities

--- study-encode/API Integration/script.js
+++ study-encode/API Integration/script.js
@@ -1,8 +1,15 @@
+
+const url = "https://www.boredapi.com/api/"
 const randomActivityBtn = document.getElementById("random-activity-btn");
 const activityParagraph = document.getElementById("activity");
 
+const activityCategorySelect = document.getElementById("activity-category");
+const activityParticipantsInput = document.getElementById("activity-participants");
+const searchActivityBtn = document.getElementById("search-activity-btn");
+const activityList = document.getElementById("activity-list");
+
 function getRandomActivity() {
-  fetch("https://www.boredapi.com/api/activity/")
+  fetch(`${url}activity/`)
     .then(response => response.json())
     .then(data => {
       activityParagraph.textContent = data.activity;
@@ -13,6 +20,46 @@ function getRandomActivity() {
 }
 
 randomActivityBtn.addEventListener("click", getRandomActivity);
+
+function searchActivities(category, participants) {
+  let apiUrl = `${url}activity/`;
+  
+  // Add category and participants query parameters to the URL if provided
+  if (category) {
+    apiUrl += `?type=${category}`;
+  }
+  if (participants) {
+    apiUrl += `&participants=${participants}`;
+  }
+  
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      // Clear the previous search results
+      activityList.innerHTML = "";
+      
+      // Display the search results
+      if (data.activity) {
+        const activityItem = document.createElement("li");
+        activityItem.textContent = data.activity;
+        activityList.appendChild(activityItem);
+      } else {
+        const noResultsItem = document.createElement("li");
+        noResultsItem.textContent = "No activities found.";
+        activityList.appendChild(noResultsItem);
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+searchActivityBtn.addEventListener("click", event => {
+  event.preventDefault();
+  const category = activityCategorySelect.value;
+  const participants = activityParticipantsInput.value;
+  searchActivities(category, participants);
+});
 
 /* axios.get("https://www.boredapi.com/api/activity/")
   .then(response => {
